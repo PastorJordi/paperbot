963ed90df041fc09d8677843cf3c2eb6f04c94b1
JordiPastor(home) <pastorc.jordi@gmail.com>
2021-04-20 22:57:28 +0200

    switching again

--- paperbot.py
+++ paperbot.py
@@ -2,8 +2,8 @@ import slack
 from flask import Flask
 from slackeventsapi import SlackEventAdapter
 import os
-
-# copy pasted from this guy
+from download_example import download_by_doi
+# template copy-pasted from this guy # https://www.youtube.com/watch?v=6gHvqXrfjuo
 
 app = Flask(__name__)
 slack_event_adapter = SlackEventAdapter(
@@ -14,17 +14,41 @@ slack_event_adapter = SlackEventAdapter(
 
 
 client = slack.WebClient(token=os.environ['SLACK_BOT_TOKEN'])
-client.chat_postMessage(channel='#testbots', text='pre-connected')
+#client.chat_postMessage(channel='#testbots', text='pre-connected')
 BOT_ID = client.api_call('auth.test')['user_id']
-client.chat_postMessage(channel='#testbots', text=f'bot id = {BOT_ID}')
+#client.chat_postMessage(channel='#testbots', text=f'bot id = {BOT_ID}')
+
+def refined_doi(wheretopost,doi):
+    ret, pth = download_by_doi(doi)
+    if ret:
+        client.files_upload(channels=wheretopost, file=pth, initial_comment='I hope is this one')
+        os.remove(pth)
+    else:
+        client.chat_postMessage(
+                        channel=wheretopost,
+                        text=f"something went wrong when retrieving the paper by doi> here's the traceback\n{pth}")
+
+#def send_help():
+#    helpstr = 'by now this just works with @mention doi [doistring]'
+
+funs = {
+    #'help' : send_help
+    'doi' : refined_doi
+}
+
 @slack_event_adapter.on('app_mention')
-def message(payload):
+def handle_msg(payload):
     event = payload.get('event', {})
     channel_id = event.get('channel')
     user_id = event.get('user')
     text= event.get('text')
-    if user_id!=BOT_ID:
-        client.chat_postMessage(channel=channel_id, text='message aknowledged\n'+text)
+    words = text.split()
+    try:
+        funs[words[1]](channel_id,words[2]) # first one is mention
+    except Exception as e: 
+        client.chat_postMessage(channel=channel_id, text=f'did not understand {words[1]} or there was an uncaught exception\n {repr(e)}')
+    # if user_id!=BOT_ID:
+    #     client.chat_postMessage(channel=channel_id, text='message aknowledged\n'+text)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0') # add port=int
